e8b5b0226a3f85437c30a2ba493c93640a9327b5
Emiliano Marchese <e.marchese1991@gmail.com>
2022-07-26 01:24:30 -0300

    ItemDetails

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemCount from './components/ItemCount'
+import { BrowserRouter } from 'react-router-dom';
 
 function App(props) {
   return (
+     
     <div className="App">
       <div>
         <Navbar />
